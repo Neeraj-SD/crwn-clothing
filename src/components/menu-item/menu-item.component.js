e874480084438fb80adc2e8d3e88a5aa07671a25
Neeraj S D <neerajsd04@gmail.com>
2022-09-25 14:31:10 +0530

    main-item styling finished

---
+++ src/components/menu-item/menu-item.component.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './menu-item.styles.scss'
+
+const MenuItem = ({ section }) => {
+
+    const styles = {
+        backgroundImage: `url(${section.imageUrl})`
+    }
+
+    return (
+        <div className={`${section.size} menu-item`}  >
+            <div className='background-image' style={styles}></div>
+            <div className='content'  >
+                <h1 className='title'>{section.title.toUpperCase()}</h1>
+                <span className='subtitle'>SHOP NOW</span>
+            </div>
+        </div>
+    )
+}
+
+export default MenuItem
