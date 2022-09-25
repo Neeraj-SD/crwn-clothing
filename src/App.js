e663ff2323fd0f9b5d89bb73a9069d312f4588fb
Neeraj S D <neerajsd04@gmail.com>
2022-09-25 19:22:43 +0530

    update menu-item to navigate with useNavigate()

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <h1>Hats Page</h1>
+)
+
 function App() {
   return (
     <div className="App">
-      <HomePage />
-    </div>
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+        <Route path='/hats' element={<HatsPage />} />
+      </Routes>
+      {/* <HomePage /> */}
+    </div >
   );
 }
 
--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router'
 
 import './menu-item.styles.scss'
 
 const MenuItem = ({ section }) => {
+
+    const navigate = useNavigate();
+
+    const onTap = () => {
+        navigate('/hats')
+    }
 
     const styles = {
         backgroundImage: `url(${section.imageUrl})`
     }
 
     return (
-        <div className={`${section.size} menu-item`}  >
+        <div className={`${section.size} menu-item`} onClick={onTap} >
             <div className='background-image' style={styles}></div>
             <div className='content'  >
                 <h1 className='title'>{section.title.toUpperCase()}</h1>
